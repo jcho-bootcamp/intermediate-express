f54e04f645b0715bb3c6a6863e1de906c4f744cb
June Hyok Cho <jcho0627@gmail.com>
2019-04-02 07:01:43 +0200

    ejs installl and render exercise

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Welcome to the Homepage!"));
+app.get("/", function (req, res) {
+  res.render("home.ejs");
+});
 
 app.listen(3000, () => console.log("Server is Listening!"));
