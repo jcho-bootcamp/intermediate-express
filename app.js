2019024a4cd31f6bbb576962a3d51c84e056463b
June Hyok Cho <jcho0627@gmail.com>
2019-04-03 06:39:26 +0200

    ejs conditionals and loops exerciase complete

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ app.get("/exercise/:thing", function (req, res) {
   res.render("love.ejs", { theThing: thing });
 });
 
+app.get("/posts", function (req, res) {
+  let posts = [
+    { title: "Post 1", author: "Suzy" },
+    { title: "My adorable pet bunny", author: "Charlie" },
+    { title: "Can you believe this Pomsky?", author: "Colt" },
+  ];
+
+  res.render("posts.ejs", { posts: posts });
+});
+
 app.listen(3000, () => console.log("Server is Listening!"));
